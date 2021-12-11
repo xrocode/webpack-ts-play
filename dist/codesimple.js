036e323f79298a070816a0791ad18b575ece581e
Ro <ro@abyss>
2021-12-11 14:35:34 -0500

    TS vs straight js

---
+++ dist/codesimple.js
@@ -0,0 +1,15 @@
+
+console.log(`inside of codesimple`);
+
+function xclicked() {
+  console.log('xclicked!');
+  let body = document.body;
+  workOnBody(body);
+}
+
+
+function workOnBody(body) {
+
+}
+
+
--- src/index.ts
+++ src/index.ts
@@ -10,15 +10,41 @@ for (let i = 0; i < 10; i++) {
 
 }
 
+console.log('im here !');
 
-let ourdiv = document.getElementById('mydiv');
-console.log(ourdiv);
+function clicked() {
+  console.log("you clicked me")
+}
 
-ourdiv.innerHTML = 'changed!'
 
-let newdiv = document.createElement('div');
-newdiv.innerHTML = 'this is a new div!'
+function xclicked() {
+  console.log('xclicked!');
+  let body = document.body;
+  workOnBody(body);
+}
 
-document.body.appendChild(newdiv)
-newdiv.id = 'cooldiv';
+
+function workOnBody(body: HTMLElement) {
+
+  //
+  
+}
+
+
+
+
+
+
+// DOM:
+
+// let ourdiv = document.getElementById('mydiv');
+// console.log(ourdiv);
+
+// ourdiv.innerHTML = 'changed!'
+
+// let newdiv = document.createElement('div');
+// newdiv.innerHTML = 'this is a new div!'
+
+// document.body.appendChild(newdiv)
+// newdiv.id = 'cooldiv';
 
