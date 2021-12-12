7feaf21b2b83582774731572f0737c683bd3233f
Ro <ro@abyss>
2021-12-11 21:44:36 -0500

    Separate ts-console folder for node14 console projects

--- ts-console/ok1.js
+++ ts-console/ok1.js
@@ -1,17 +1,17 @@
+"use strict";
 console.log('lol');
 function myfunc(z) {
-    for (var i = 0; i < z.length; i++) {
+    for (let i = 0; i < z.length; i++) {
         console.log(z[i]);
     }
     return z.length;
 }
-var arr = ['abibi', 'ififi', 'ddodo'];
+let arr = ['abibi', 'ififi', 'ddodo'];
 myfunc(arr);
-var al = arr.push('7');
+let al = arr.push('7');
 console.log('array lenght:', al);
 myfunc(arr);
-for (var _i = 0, arr_1 = arr; _i < arr_1.length; _i++) {
-    var x = arr_1[_i];
+for (let x of arr) {
     console.log(x);
 }
 console.log(arr);
